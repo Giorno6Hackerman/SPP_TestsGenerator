40008a51ccf4f1049922b492530b97870656bf38
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-12-13 19:39:21 +0300

    Added testclass

---
+++ TestsGenerator/TestsGeneratorLibrary/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace TestsGeneratorLibrary
+{
+    public class TestClass
+    {
+        private StringBuilder _text = new StringBuilder();
+
+        public string Name { get; set; }
+        public string Text 
+        {
+            get 
+            {
+                return _text.ToString();    
+            }
+            set
+            {
+                _text.Append(value);
+            } 
+        }
+    }
+}
