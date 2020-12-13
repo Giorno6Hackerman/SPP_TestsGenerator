5fdc2a011764acaf47e17c84c350e42c9c8311ed
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-12-13 19:56:12 +0300

    Fixed generator again

--- TestsGenerator/TestsGeneratorLibrary/TestsGenerator.cs
+++ TestsGenerator/TestsGeneratorLibrary/TestsGenerator.cs
@@ -5,13 +5,6 @@ namespace TestsGeneratorLibrary
 {
     public class TestsGenerator
     {
-        private int _maxTaskCount;
-
-        public TestsGenerator(int maxTasksCount)
-        {
-            _maxTaskCount = maxTasksCount;
-        }
-
         public static async Task<string> GenerateTest(string file)
         {
             return null;
