ffc6d46152e2645cc674723379279cfaa1e5fc12
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-12-13 19:39:48 +0300

    Added io methods

--- TestsGenerator/TestsGeneratorConsole/Program.cs
+++ TestsGenerator/TestsGeneratorConsole/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using TestsGeneratorLibrary;
 
 namespace TestsGeneratorConsole
 {
     class Program
     {
-        private static List<string> _files = new List<string>();
-        private static string _destinationFolder;
-        private static int _maxTasksCount;
-        private static int _maxLoadedFilesCount;
-        private static int _maxStoredFilesCount;
-
         static void Main(string[] args)
         {
+            List<string> _files = new List<string>();
             Console.WriteLine("Enter file's path(or 0 to finish): ");
+            // + check file existing
             string path;
             path = Console.ReadLine();
             while (path != "0")
@@ -23,20 +22,35 @@ namespace TestsGeneratorConsole
             }
 
             Console.WriteLine("Enter max tasks count: ");
-            _maxTasksCount = Int32.Parse(Console.ReadLine());
+            int _maxTasksCount = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter max loaded files count: ");
-            _maxLoadedFilesCount = Int32.Parse(Console.ReadLine());
-
+            int _maxLoadedFilesCount = Int32.Parse(Console.ReadLine());
+            double x = 1 + 1;
             Console.WriteLine("Enter max stored files count: ");
-            _maxStoredFilesCount = Int32.Parse(Console.ReadLine());
+            int _maxStoredFilesCount = Int32.Parse(Console.ReadLine());
 
+            // + check dir existing
             Console.WriteLine("Enter destination folder path: ");
-            _destinationFolder = Console.ReadLine();
+            string _destinationFolder = Console.ReadLine();
 
             // call loader(_files, _maxLoadedFilesCount)
+            // 
             // call generator(fromLoader, _maxTasksCount)
             // call writer(fromgenerator, _destinationFolder, _maxStoredFilesCount)
+        }
+
+        public async Task<string> LoadText(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        public async Task WriteFile()
+        {
+            // await WriteAsync
         }
     }
 }
